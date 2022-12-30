66174d9c1855b0e05451f8fcb6bc709ec95a0879
justdreamers <2534405346@qq.com>
2022-12-30 10:02:54 +0800

    。

---
+++ test_9_27/test_9_27/test.c
@@ -0,0 +1,55 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+int main()
+{
+
+	return 0;
+}
+//int main()
+//{
+//	int ret = 0;
+//	char password[20] = { 0 };
+//	printf("请输入密码:>");
+//	scanf("%s",password);
+//	getchar();
+//	printf("请确认(Y/N):>");
+//	   ret = getchar();
+//	if (ret == 'Y')
+//	{
+//		printf("确认成功\n");
+//	}
+//	else
+//	{
+//		printf("确认失败\n");
+//	}
+//		return 0;
+//}
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+//int  add(int x, int y)
+//{
+//	int z = 0;
+//	z = x +y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 23;
+//	int num1 = 12;
+//	int num2 = 43;
+//	int sum= 0;
+//	
+//	sum= add(a, b);
+//	sum= add(num1,num2);
+//	printf("sum=%d sum= %d\n", sum,sum);
+//	return 0;
+//}

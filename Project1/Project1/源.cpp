b5f549cfbea1a61aa9e8fd54ed797817843e6591
justdreamers <2534405346@qq.com>
2023-01-03 21:10:04 +0800

    。

---
+++ Project1/Project1/源.cpp
@@ -0,0 +1,87 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int arr[10] = { 0 };
+	int sz = 0;
+	printf("%d\n", sizeof(arr));
+	sz = sizeof(arr) / sizeof(arr[0]);
+	printf("sz=%d", sz);
+	/*int a = 10;
+	printf("%d\n", sizeof(a));*/
+	return  0;
+}
+//{
+	//int a = 10;
+	//printf("%d\n", a);
+	//printf("%d\n", !a);
+//{/*
+//	int arr[10] = { 1, 2, 3 ,4 ,5, 6 ,7 ,8, 9 ,10 };	
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d ", arr[i]);
+//		i++;
+//	}*/
+	//printf("%d\n", arr[4]);//利用下标访问元素
+	/*return 0;*/
+//}
+
+//}
+//{/*
+//	int line = 0;
+//	printf("加入比特\n");
+//
+//	while (line < 20000)
+//	{
+//	 printf("敲一行代码:%d\n", line);
+//     line++;
+//	}
+//	if (line >= 20000)
+//		printf("好offer\n");
+//	return 0;*/
+//}
+	
+
+/*int  input = 0;
+	printf("上大学\n");
+	printf("你要好好学习吗?（1/0）>:");
+	scanf("%d", &input);
+	if (input == 1)
+	printf("好工作\n");
+	else
+		printf("卖红薯\n");
+
+	return 0;*/
+
+
+
+
+
+	//printf("%c\n", '\x61');
+	//printf("%c\n", '\x36');
+	//printf("%d\n",strlen("c:\test\32\test.c"));
+	//printf("abc\n");
+	//return 0;
+
+
+	//char arr1[] = "abc";
+	//char arr2[] = { 'a','b','c',0};
+	//printf("%s\n", (arr1));
+	//printf("%s\n",  (arr2));
+	//return 0;
+
+	//int num1 = 0;
+	//int num2 = 0;
+	//int num3 = 0;
+	// int sum = 0;
+	//scanf("%d%d%d", &num1, &num2,&num3);
+	//sum = num1 +num2+ num3;
+	//printf("%d\n", sum);
+	//return 0;
+//}
+	
+
+	
